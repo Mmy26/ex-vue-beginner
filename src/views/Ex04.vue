af003745af9c3ecf77c2956a3fe27b8a4d3ead3f
Mmy26 <yu.b1994masamura@gmail.com>
2022-02-07 16:16:59 +0900

    演習４追加修正（1000の位でカンマを表示）

--- src/views/Ex04.vue
+++ src/views/Ex04.vue
@@ -3,8 +3,12 @@
     <div><input type="number" value="item1" v-model.number="item1" /></div>
     <div><input type="number" value="item2" v-model.number="item2" /></div>
     <div><input type="number" value="item3" v-model.number="item3" /></div>
-    <div>税抜き価格：{{ item1 + item2 + item3 }}円</div>
-    <div>税込み価格：{{ Math.floor((item1 + item2 + item3) * 1.1) }}円</div>
+    <div>税抜き価格：{{ (item1 + item2 + item3).toLocaleString() }}円</div>
+    <div>
+      税込み価格：{{
+        Math.floor((item1 + item2 + item3) * 1.1).toLocaleString()
+      }}円
+    </div>
   </div>
 </template>
 
